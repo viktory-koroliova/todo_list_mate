adab203a3436280e805894f5943d9859d4bfccc4
Vika <viktorykoroliova@gmail.com>
2023-02-07 19:03:33 +0200

    Added urls to all views and functions

---
+++ todo/urls.py
@@ -0,0 +1,41 @@
+from django.urls import path
+
+from todo.views import (
+    TagListView,
+    TaskListView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TaskCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    TagCreateView,
+    change_is_done_status,
+)
+
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path(
+        "tasks/<int:pk>/change_status/",
+        change_is_done_status,
+        name="change-status"
+    ),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+]
+
+
+app_name = "todo"
